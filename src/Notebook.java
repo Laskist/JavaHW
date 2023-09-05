b3958817a5d70618a1f55a5168fc12bb6e119248
Laskist <111230844+Laskist@users.noreply.github.com>
2023-09-05 08:09:52 +0300

    Update Notebook.java

--- src/Notebook.java
+++ src/Notebook.java
@@ -17,16 +17,32 @@ public class Notebook {
         return ram;
     }
 
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
     public int getHdd() {
         return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
     }
 
     public String getOs() {
         return os;
     }
 
+    public void setOs(String os) {
+        this.os = os;
+    }
+
     public String getColor() {
         return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
 
